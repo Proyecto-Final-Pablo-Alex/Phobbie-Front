b8b00aa1b9a79d534027e77f4ff97bf8c51822a0
alescofet <alex.escofet96@gmail.com>
2021-05-11 12:56:15 +0200

    Signup form modded

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -41,6 +41,14 @@ class Signup extends React.Component {
   }
 
   render() {
+    const provincias = ['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres',
+    'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara',
+    'Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra',
+    'Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona',
+    'Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza']
+
+    const allOptions=provincias.map((provincia)=>{return(<option>{provincia}</option>)})
+    
     return this.state.successSignUp ? (
       <Redirect to="/login" />
     ) : (
@@ -61,7 +69,20 @@ class Signup extends React.Component {
             onChange={(event) => this.handleChange(event)}
           />
 
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            name="age"
+            onChange={(event) => this.handleChange(event)}
+          />
+
+          <label htmlFor="location">Location</label>
+          <select name="location" onChange={(event) => this.handleChange(event)}>
+            {allOptions}
+          </select>
+
           <button>Sign up</button>
+          
         </form>
         {this.state.errorMessage ? <p>Este usuario ya existe</p> : null}
       </div>
