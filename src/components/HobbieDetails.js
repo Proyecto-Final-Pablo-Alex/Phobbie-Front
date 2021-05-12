cefc4ad522a084000b03c6bf7193d4b0bb9f0a69
alescofet <alex.escofet96@gmail.com>
2021-05-12 13:14:41 +0200

    HobbieDetails component created

---
+++ src/components/HobbieDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
+class HobbieDetails extends React.Component {
+    render(){
+        return(
+        <div>
+            
+        </div>
+        )
+    }
+}
+
+export default HobbieDetails;
