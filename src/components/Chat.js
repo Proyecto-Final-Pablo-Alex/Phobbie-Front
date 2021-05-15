5b0e7797b838c3ec4ec817ef23cb94b321f90bf1
PabloCabreraR <pablocabrera2g@gmail.com>
2021-05-15 19:04:08 +0200

    doing chat

---
+++ src/components/Chat.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
+import io from 'socket.io-client'
+
+class Chat extends React.Component {
+    state={
+        chat:{
+            participants:[],
+            room:'',
+            messages:[],
+        },
+        message:'',
+        loaded:false,
+    }
+
+    componentDidMount(){
+        axios({
+            method: "get",
+            url: `http://localhost:5000/return-chat/${this.props.match.params.id}`,
+            withCredentials: true
+          })
+          .then(result => {
+              console.log(result.data)
+              const stateCopy = {...this.state}
+              stateCopy.chat = result.data.result
+              stateCopy.loaded = true
+              this.setState(stateCopy)
+          })
+          .catch(error => {
+              console.log(error)
+          })  
+    }
+
+    componentDidUpdate(){
+        axios({
+            method: "get",
+            url: `http://localhost:5000/return-chat/${this.props.match.params.id}`,
+            withCredentials: true
+          })
+          .then(result => {
+              console.log(result.data)
+              const stateCopy = {...this.state}
+              stateCopy.chat = result.data.result
+              stateCopy.loaded = true
+            if (result.data.result.messages.length !== this.state.chat.messages.length){
+              this.setState(stateCopy)}
+          })
+          .catch(error => {
+              console.log(error)
+          }) 
+    }
+
+
+    handleChange(event) { 
+        const {value, name } = event.target;
+        this.setState({
+          ...this.state,
+          message: {[name]: value, username: req.params.user.username, date: new Date()},
+        });
+        console.log(this.state.user);  
+      }
+
+
+    render(){
+        const messages = this.state.chat.messages.map((message,index)=>{
+           <div>
+            <p>{message.username}: {message.message}</p>
+            <p>{message.date}</p>
+           </div>
+        })
+        const friend = this.state.chat.participants.filter()
+        return(
+            <div>
+                <h2>Chatting with {}</h2>
+                <div>
+                    {messages}
+                </div>
+                <div>
+                    <form action="">
+                        <input type="text" name="message" onChange=""/>
+                        <button onClick="">Send</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+export default Chat;
