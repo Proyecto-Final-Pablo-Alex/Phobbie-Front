7d3fd5c76edd393da60a3512bc4a65e75c45fdfc
alescofet <alex.escofet96@gmail.com>
2021-05-20 10:17:37 +0200

    merged the front into the back

--- src/components/BigScreen.js
+++ src/components/BigScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 class BigScreen extends React.Component{
     render(){
+        const tuerca = 'https://res.cloudinary.com/hobbiesphere/image/upload/v1621495656/tuerca_zkxbhg.png'
         return (
             <div className="BigScreen">
                 <div>
@@ -9,10 +10,10 @@ class BigScreen extends React.Component{
                     <p>We are working on making the Desktop version available.</p>
                     <p>Please open the app with a mobile phone.</p>
                     <div>
-                    <img className='gear1' src='https://cdn.icon-icons.com/icons2/1788/PNG/512/nuticon_114497.png' />
-                    <img className='gear2' src='https://cdn.icon-icons.com/icons2/1788/PNG/512/nuticon_114497.png' />
-                    <img className='gear1' src='https://cdn.icon-icons.com/icons2/1788/PNG/512/nuticon_114497.png' />
-                    <img className='gear2' src='https://cdn.icon-icons.com/icons2/1788/PNG/512/nuticon_114497.png' />
+                    <img className='gear1' src={tuerca}/>
+                    <img className='gear2' src={tuerca}/>
+                    <img className='gear1' src={tuerca}/>
+                    <img className='gear2' src={tuerca}/>
                     </div>
                     <img src="https://res.cloudinary.com/hobbiesphere/image/upload/v1621265220/Logo_Phobbie_solo_nombre_negro_lxq426.png" className="phobbie-logo" />
                 </div>
